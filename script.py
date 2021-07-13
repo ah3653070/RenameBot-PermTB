88a9bba1b815851df865d3920cd241002d756835
ah3653070 <81767951+ah3653070@users.noreply.github.com>
2021-07-13 08:55:11 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ A Simple File Renamer Bot With  Thumbnail support' update @Royalbotz!
 
 <b>Send me any Telegram file and choose appropriate option! </b>"""
 
@@ -7,13 +7,13 @@ class script(object):
     UPGRADE_TEXT = "CONTACT @prgofficial"
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Royalbotz **"
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully ⚠️"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """ Contact @tgbotschat😅
     
 1. Send me any Telegram File.
 2. Choose appropriate option."""
